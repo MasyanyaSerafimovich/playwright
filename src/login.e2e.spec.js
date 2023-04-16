8caa63076ec3833a581968109c94970c90aed044
Sima <MasyanyaSerafimovich>
2023-04-17 02:43:11 +0300

    added complexForm test

--- src/login.e2e.spec.js
+++ src/login.e2e.spec.js
@@ -7,16 +7,18 @@ let page, browser, context
 const selectors = {
   login: 'input[name=email]',
   password: 'input[name=password]',
-  loginBtn: 'input[type=submit]',
+  loginSelect: '[name=selectLogin]',
+  loginCheckBox: '[name=loveForm]',
+  loginBtn: '[type=submit]',
   loginSuccess: '.container h1'
 }
 
 const credentials = {
-  login: 'simpleForm@authenticationtest.com',
+  login: 'complex@authenticationtest.com',
   password: 'pa$$w0rd'
 }
 
-describe('authenticationtest:login', () => {
+describe('Login page', () => {
   beforeEach(async function() {
     browser = await playwright.chromium.launch({
       headless: false,
@@ -24,27 +26,47 @@ describe('authenticationtest:login', () => {
     });
       
     context = await browser.newContext()
-    page = await context.newPage('https://authenticationtest.com/simpleFormAuth/')
+    page = await context.newPage('https://authenticationtest.com/complexAuth/')
   })
 
   afterEach(async function() {
-    await page.screenshot({ path: `schreenshots/${this.currentTest.title.replace(/\s+/g, '_')}.png` })
+    await page.screenshot({ path: `screenshots/${this.currentTest.title.replace(/\s+/g, '_')}.png` })
     await browser.close()
   })
 
   it('should exists', async() => {
-    await page.goto('https://authenticationtest.com/simpleFormAuth');
+    await page.goto('https://authenticationtest.com/complexAuth/');
     
     const title = await page.title()
     expect(title).to.equal('Authentication Test')
   })
 
-  it('should successfully login and redirect to main page', async() => {
-    await page.goto('https://authenticationtest.com/simpleFormAuth');
+  it(`should not login, if selector said Don't Log Me In`, async() => {
+    await page.goto('https://authenticationtest.com/complexAuth/');
 
     await page.locator(selectors.login).fill(credentials.login);
     await page.locator(selectors.password).fill(credentials.password);
 
+    await page.click(selectors.loginCheckBox);
+    await page.click(selectors.loginBtn);
+
+    await page.waitForLoadState('networkidle');
+    
+    const title = await page.locator(selectors.loginSuccess).textContent();
+
+    expect(title).to.equal('Login Failure')
+  })
+
+  it('should successfully login and redirect to main page', async() => {
+    await page.goto('https://authenticationtest.com/complexAuth/');
+
+    await page.locator(selectors.login).fill(credentials.login);
+    await page.locator(selectors.password).fill(credentials.password);
+
+    await page.click(selectors.loginSelect)
+    await page.locator('[value=yes]').click();
+
+    await page.click(selectors.loginCheckBox);
     await page.click(selectors.loginBtn);
 
     await page.waitForLoadState('networkidle');
