f2d0022154f7aa02dcd34e8a0dd4136943d9eb93
Sima <MasyanyaSerafimovich>
2023-04-17 03:16:30 +0300

    added complexForm test

--- src/login.e2e.spec.js
+++ src/login.e2e.spec.js
@@ -7,10 +7,9 @@ let page, browser, context
 const selectors = {
   login: 'input[name=email]',
   password: 'input[name=password]',
-  loginSelect: '[name=selectLogin]',
   loginCheckBox: '[name=loveForm]',
   loginBtn: '[type=submit]',
-  loginSuccess: '.container h1'
+  h1Position: '.container h1'
 }
 
 const credentials = {
@@ -20,6 +19,7 @@ const credentials = {
 
 describe('Login page', () => {
   beforeEach(async function() {
+
     browser = await playwright.chromium.launch({
       headless: false,
       slowMo: 2000
@@ -35,44 +35,74 @@ describe('Login page', () => {
   })
 
   it('should exists', async() => {
+
+    // Go to authentication form
     await page.goto('https://authenticationtest.com/complexAuth/');
     
-    const title = await page.title()
-    expect(title).to.equal('Authentication Test')
+    const title = await page.locator(selectors.h1Position).textContent();
+    expect(title).to.equal('Complex Form Authentication')
   })
 
-  it(`should not login, if selector said Don't Log Me In`, async() => {
-    await page.goto('https://authenticationtest.com/complexAuth/');
+  it(`should not login, if selector option Don't Log Me In chosen`, async() => {
 
+    await page.goto('https://authenticationtest.com/complexAuth/');
     await page.locator(selectors.login).fill(credentials.login);
     await page.locator(selectors.password).fill(credentials.password);
 
+    // Select option "Don't Log Me In"
+    await page.selectOption('[name="selectLogin"]', 'no');
+
     await page.click(selectors.loginCheckBox);
     await page.click(selectors.loginBtn);
-
     await page.waitForLoadState('networkidle');
     
-    const title = await page.locator(selectors.loginSuccess).textContent();
+    const title = await page.locator(selectors.h1Position).textContent();
+    expect(title).to.equal('Login Failure')
+  })
 
+  it(`should not login, if checkbox do not selected`, async() => {
+
+    await page.goto('https://authenticationtest.com/complexAuth/');
+    await page.locator(selectors.login).fill(credentials.login);
+    await page.locator(selectors.password).fill(credentials.password);
+    await page.selectOption('[name="selectLogin"]', 'yes');
+
+    // Checkbox do not selected
+    await page.click(selectors.loginBtn);
+    await page.waitForLoadState('networkidle');
+    
+    const title = await page.locator(selectors.h1Position).textContent();
+    expect(title).to.equal('Login Failure')
+  })
+
+  it(`should not login, if selector option Don't Log Me In chosen AND checkbox do not selected`, async() => {
+
+    await page.goto('https://authenticationtest.com/complexAuth/');
+    await page.locator(selectors.login).fill(credentials.login);
+    await page.locator(selectors.password).fill(credentials.password);
+
+    // Select option "Don't Log Me In"
+    await page.selectOption('[name="selectLogin"]', 'no');
+
+    // Checkbox do not selected
+    await page.click(selectors.loginBtn);
+    await page.waitForLoadState('networkidle');
+    
+    const title = await page.locator(selectors.h1Position).textContent();
     expect(title).to.equal('Login Failure')
   })
 
   it('should successfully login and redirect to main page', async() => {
-    await page.goto('https://authenticationtest.com/complexAuth/');
 
+    await page.goto('https://authenticationtest.com/complexAuth/');
     await page.locator(selectors.login).fill(credentials.login);
     await page.locator(selectors.password).fill(credentials.password);
-
-    await page.click(selectors.loginSelect)
-    await page.locator('[value=yes]').click();
-
+    await page.selectOption('[name="selectLogin"]', 'yes');
     await page.click(selectors.loginCheckBox);
     await page.click(selectors.loginBtn);
-
     await page.waitForLoadState('networkidle');
     
-    const title = await page.locator(selectors.loginSuccess).textContent();
-
+    const title = await page.locator(selectors.h1Position).textContent();
     expect(title).to.equal('Login Success')
   })
 })
